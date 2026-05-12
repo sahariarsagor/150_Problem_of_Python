43c9b4cbaec05309f9e66dcc1ee255fb4aa9a386
Sahariar Sagor <contactsahariar@gmail.com>
2026-05-12 23:14:05 +0600

    Delete List directory

--- List/list.py
+++
@@ -1,9 +0,0 @@
-'''
-    A list in Python is a collection of items (elements) that can be of different data types. Lists are ordered, mutable (you can change the values), and allow duplicate values. Lists are created using square brackets ([]).
-'''
-
-#Creating a list 
-s = ["sahariar", 5, 10, 20, True, "Sorna"];
-
-# accessing a list using index number. 
-print(s[3]);
